5088be75efffb721c67233bb4149627d0e7a4b73
pmacedo-19 <wardian@protonmail.com>
2022-05-28 19:47:10 +0100

    19:47

--- src/app/business/Business-detail/Business-detail.component.ts
+++ src/app/business/Business-detail/Business-detail.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import businessesData from '../../../data/businesses.json';
+import { IBusiness } from '../IBusiness.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-Business-detail',
@@ -8,8 +11,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class BusinessDetailComponent implements OnInit {
   public businessId!: number;
+  @Input() business : any;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  businessesList:{ Id: number; Name: string; Type: string; Discription: string; Image?: string}[] = businessesData;
+
+  constructor(private route: ActivatedRoute, private router: Router, private http:HttpClient) { }
 
   ngOnInit() {
     const Id = 'Id';
@@ -17,7 +23,21 @@ export class BusinessDetailComponent implements OnInit {
   }
 
   onOpenReview() {
-    this.router.navigate(['business-review', ])
+    this.router.navigate(['business-review', this.businessId])
   }
+
+  //const filteredData = businessesList
+
+  // getByIdInit(): void {
+  //   this.http.get('data/businesses.json').subscribe(
+  //     data=>{
+  //       this.Businesses=data;
+  //       console.log(data)
+  //     }
+  //   )
+  // }
+
+
+
 
 }
--- src/app/business/IBusiness.interface.ts
+++ src/app/business/IBusiness.interface.ts
@@ -2,4 +2,6 @@ export interface IBusiness {
   Id: number;
   Name: string;
   Type: string;
+  Discription: string;
+  Image?: string;
 }
